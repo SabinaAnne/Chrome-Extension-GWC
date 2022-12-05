b408f319d0f980cd1d001f4e1585b543e7a7d52c
reutshiran <114440997+reutshiran@users.noreply.github.com>
2022-12-05 13:00:34 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,2 @@
 console.log("This is a popup!")
 
-document.getElementById("test").onclick = function() {
-    document.getElementById("main-card").style.height = "60rem"
-    document.getElementById("main-card").style.width = "60rem"
-}
